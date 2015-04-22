7081a1b955d2dfeba8c23df9368d3de907989069
Roark Brewster <roarkb@Roark-Brewsters-MacBook.local>
2015-04-21 22:39:06 -0700

    better tests

--- test.rb
+++ test.rb
@@ -2,78 +2,56 @@
 
 require "./binary_search_tree"
 
-# ints
-puts "\nINTS:\n\n"
-p t = BinarySearchTree.new(10)
-puts
-t.insert(5)
-p t
-puts
-t.insert(15, 1, 8, 12, 20, 7, 14)
-p t
-puts
-t.insert("foo")
-puts
-p t.lookup(5)
-p t.lookup()
-puts
-p t.lookup(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-puts
-puts "breadth-first list:"
-p t.list_breadth_first
-puts
-puts "preorder list:"
-p t.list_preorder
-puts
-puts "inorder list:"
-p t.list_inorder
-puts
-puts "postorder list:"
-p t.list_postorder
-puts
-puts "all paths root to leaf:"
-p t.paths
-puts
-p t.right.left.right.value
+def test(title, values, lookup_terms)
+  bad_data = values.first.class == String ? 1 : "foo"
+  
+  puts "\n#{title.upcase}:\n\n"
+  p t = BinarySearchTree.new(values.shift)
+  puts
+  t.insert(values.shift)
+  p t
+  puts
+  t.insert(*values)
+  p t
+  puts
+  t.insert(bad_data)
+  puts
+  p t.lookup(values.first)
+  p t.lookup()
+  puts
+  p t.lookup(*lookup_terms)
+  puts
+  puts "breadth-first list:"
+  p t.list_breadth_first
+  puts
+  puts "depth-first preorder list:"
+  p t.list_preorder
+  puts
+  puts "depth-first inorder list:"
+  p t.list_inorder
+  puts
+  puts "depth-first postorder list:"
+  p t.list_postorder
+  puts
+  puts "all paths root to leaf:"
+  p t.paths
+  puts
+end
 
-# strings
-puts "\nSTRINGS:\n\n"
-p t = BinarySearchTree.new("foo")
-puts
-t.insert("bar")
-p t
-puts
-t.insert("gaz", "fub", "mif", "sup")
-p t
-puts
-t.insert(5)
-puts
-p t.lookup("qwe", "foo", "asd", "gaz", "zxc", "fub", "wer", "mif", "sup")
-puts
-puts "breadth-first list:"
-p t.list_breadth_first
-puts
-puts "preorder list:"
-p t.list_preorder
-puts
-puts "inorder list:"
-p t.list_inorder
-puts
-puts "postorder list:"
-p t.list_postorder
-puts
-puts "all paths root to leaf:"
-p t.paths
-puts
-p t.right.left.value
+test(
+  "ints", 
+  [ 10, 5, 1, 8, 12, 20, 7, 14 ],
+  (0..25).to_a
+)
 
-# other
-puts "\nOTHER:\n\n"
-p t = BinarySearchTree.new([ 1, 2, 3 ])
-puts
-t.insert(4)
-puts
-t.insert([0, 1, 2], [2, 3, 4])
-p t
-#t = BinarySearchTree.new({})
-puts
+test(
+  "strings", 
+  [ "foo", "bar", "gaz", "fub", "zuz", "guf", "kik", "pup" ], 
+  [ "foo", "bar", "mmm", "gaz", "fub", "yyy", "zuz", "guf", "kik", "hhh", "pup" ]
+) 
+
+test(
+  "arrays",
+  [ [ 5, 6, 7 ], [ 8, 9, 10 ], [ 2, 3, 4 ], [ 6, 7, 8 ], [ 3, 4, 5 ], [ 7, 8, 9 ] ],
+  [ [ 5, 6, 7 ], [ 8, 9, 10 ], [ 2, 3, 4 ], [ 6, 7, 8 ], [ 3, 4, 5 ], [ 7, 8, 9 ], [ 9, 9, 9 ], [ 8, 8, 8 ], [ 7, 7, 7] ]
+)
